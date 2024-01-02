87a45ef0bdcc257c36ebf37de701d87c9ae5d899
shirpalang <shirpalang@gmail.com>
2024-01-03 01:49:12 +0330

    coding jump Featureگ گ

--- src/js/app.js
+++ src/js/app.js
@@ -2,25 +2,77 @@ const zamin = document.getElementById('zamin')
 const character = document.getElementById('character')
 
 let zaminPosition = 1
-const frames = ['./public/images/character1.png' , './public/images/character2.png' , './public/images/character3.png' , './public/images/character2.png']
+const frames = ['./public/images/character1.png', './public/images/character2.png', './public/images/character3.png', './public/images/character2.png']
+
+let isJump = false
 
 
-setInterval(()=> {
+setInterval(() => {
     zamin.innerHTML += `<img src="public/images/zamin.jpg" alt="">`
-}, 700)
+}, 600)
 
 setInterval(() => {
     zaminPosition = zaminPosition + 10
     zamin.style.left = `-${zaminPosition}px`
-    
+
 }, 20)
 
 let index = 0
-setInterval(()=> {
-    if(index === frames.length - 1){
-        index = 0 
+setInterval(() => {
+    if (index === frames.length - 1) {
+        index = 0
     } else {
         index++
     }
     character.children[0].src = frames[index]
 }, 130)
+
+document.body.addEventListener('keydown', e => {
+
+    let jumpPosition = 153
+    let isUp = false
+
+
+    if (e.key === ' ' && !isJump) {
+        jumpHandler(jumpPosition, isUp)
+    }
+
+    if (e.key === ' ') {
+        isJump = true
+    }
+
+    setTimeout(() => {
+        isJump = false
+    }, 1100);
+
+})
+
+const jumpHandler = (position , isUp) => {
+    const jumpDown = interval => {
+        let toDown = setInterval(() => {
+            if (position === 300) {
+                clearInterval(interval)
+                isUp = true
+            }
+
+            if (isUp && position !== 153) {
+                position -= 1
+                character.style.bottom = `${position}px`
+            }
+
+            if (position === 153) {
+                clearInterval(toDown)
+            }
+        }, 1)
+    }
+
+    let jumpUp = setInterval(() => {
+        if (position !== 300) {
+            position += 1
+            character.style.bottom = `${position}px`
+            return false
+        }
+    }, 1)
+
+    jumpDown(jumpUp)
+}
